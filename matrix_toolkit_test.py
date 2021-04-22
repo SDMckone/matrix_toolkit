3f0005dac15dbe8dcd97a9f18262be825cf4ef4d
sean <seandmckone@hotmail.com>
2021-04-22 17:44:25 -0400

    Completed matrix_toolkit test_add_matrices and test_subtract_matrices

--- matrix_toolkit_test.py
+++ matrix_toolkit_test.py
@@ -2,81 +2,101 @@ import unittest
 import matrix_toolkit
 
 class TestMatrixToolkit(unittest.TestCase):
+
+    # Test matrix_toolkit's add_matrices method
     def test_add_matrices(self): 
 
         # Test adding two matrices with different dimensions
-        # TODO
+        matrix_A1 = [[1,2],[3,4]]
+        matrix_A2 = [[1,2],[3,4],[5,6]]
+
+        try:
+            matrix_toolkit.add_matrices(matrix_A1, matrix_A2)
+            self.fail()
+        except Exception as e:
+            self.assertEqual("Incompatible matrices.", e.args[0])
 
         # Test adding two 2x2 matrices
-        matrix_A1 = [[1,2],[3,4]]
-        matrix_A2 = [[1,2],[3,4]]
+        matrix_B1 = [[1,2],[3,4]]
+        matrix_B2 = [[1,2],[3,4]]
 
-        resultant_matrix_A = [[2,4],[6,8]]
-
-        self.assertEqual(matrix_toolkit.add_matrices(matrix_A1, matrix_A2), resultant_matrix_A)
-
-    
-        # Test adding two 3x3 matrices
-        matrix_B1 = [[1,2,3],[4,5,6],[7,8,9]]
-        matrix_B2 = [[1,2,3],[4,5,6],[7,8,9]]
-
-        resultant_matrix_B = [[2,4,6],[8,10,12],[14,16,18]]
+        resultant_matrix_B = [[2,4],[6,8]]
 
         self.assertEqual(matrix_toolkit.add_matrices(matrix_B1, matrix_B2), resultant_matrix_B)
+        
+    
+        # Test adding two 3x3 matrices
+        matrix_C1 = [[1,2,3],[4,5,6],[7,8,9]]
+        matrix_C2 = [[1,2,3],[4,5,6],[7,8,9]]
 
-        # Test adding two 5x2 matrices
-        matrix_C1 = [[1,2],[3,4],[5,6],[7,8],[9,10]]
-        matrix_C2 = [[1,2],[3,4],[5,6],[7,8],[9,10]]
-
-        resultant_matrix_C = [[2,4],[6,8],[10,12],[14,16],[18,20]]
+        resultant_matrix_C = [[2,4,6],[8,10,12],[14,16,18]]
 
         self.assertEqual(matrix_toolkit.add_matrices(matrix_C1, matrix_C2), resultant_matrix_C)
 
-        # Test adding two 2x5 matrices
-        matrix_D1 = [[1,2,3,4,5],[6,7,8,9,10]]
-        matrix_D2 = [[1,2,3,4,5],[6,7,8,9,10]]
+        # Test adding two 5x2 matrices
+        matrix_D1 = [[1,2],[3,4],[5,6],[7,8],[9,10]]
+        matrix_D2 = [[1,2],[3,4],[5,6],[7,8],[9,10]]
 
-        resultant_matrix_D = [[2,4,6,8,10],[12,14,16,18,20]]
+        resultant_matrix_D = [[2,4],[6,8],[10,12],[14,16],[18,20]]
 
         self.assertEqual(matrix_toolkit.add_matrices(matrix_D1, matrix_D2), resultant_matrix_D)
 
+        # Test adding two 2x5 matrices
+        matrix_E1 = [[1,2,3,4,5],[6,7,8,9,10]]
+        matrix_E2 = [[1,2,3,4,5],[6,7,8,9,10]]
+
+        resultant_matrix_E = [[2,4,6,8,10],[12,14,16,18,20]]
+
+        self.assertEqual(matrix_toolkit.add_matrices(matrix_E1, matrix_E2), resultant_matrix_E)
+
+    # Test matrix_toolkit's subtract_matrices method
     def test_subtract_matrices(self): 
         # Test subtracting two matrices with different dimensions
-        # TODO
+        matrix_A1 = [[1,2],[3,4]]
+        matrix_A2 = [[1,2],[3,4],[5,6]]
+
+        try:
+            matrix_toolkit.subtract_matrices(matrix_A1, matrix_A2)
+            self.fail()
+        except Exception as e:
+            self.assertEqual("Incompatible matrices.", e.args[0])
 
         # Test subtracting two 2x2 matrices
-        matrix_A1 = [[1,2],[3,4]]
-        matrix_A2 = [[1,2],[3,4]]
+        matrix_B1 = [[1,2],[3,4]]
+        matrix_B2 = [[1,2],[3,4]]
 
-        resultant_matrix_A = [[0,0],[0,0]]
-
-        self.assertEqual(matrix_toolkit.subtract_matrices(matrix_A1, matrix_A2), resultant_matrix_A)
-
-    
-        # Test adding two 3x3 matrices
-        matrix_B1 = [[1,2,3],[4,5,6],[7,8,9]]
-        matrix_B2 = [[1,2,3],[4,5,6],[7,8,9]]
-
-        resultant_matrix_B = [[0,0,0],[0,0,0],[0,0,0]]
+        resultant_matrix_B = [[0,0],[0,0]]
 
         self.assertEqual(matrix_toolkit.subtract_matrices(matrix_B1, matrix_B2), resultant_matrix_B)
 
-        # Test adding two 5x2 matrices
-        matrix_C1 = [[1,2],[3,4],[5,6],[7,8],[9,10]]
-        matrix_C2 = [[1,2],[3,4],[5,6],[7,8],[9,10]]
 
-        resultant_matrix_C = [[0,0],[0,0],[0,0],[0,0],[0,0]]
+        # Test adding two 3x3 matrices
+        matrix_C1 = [[1,2,3],[4,5,6],[7,8,9]]
+        matrix_C2 = [[1,2,3],[4,5,6],[7,8,9]]
+
+        resultant_matrix_C = [[0,0,0],[0,0,0],[0,0,0]]
 
         self.assertEqual(matrix_toolkit.subtract_matrices(matrix_C1, matrix_C2), resultant_matrix_C)
 
-        # Test adding two 2x5 matrices
-        matrix_D1 = [[1,2,3,4,5],[6,7,8,9,10]]
-        matrix_D2 = [[1,2,3,4,5],[6,7,8,9,10]]
 
-        resultant_matrix_D = [[0,0,0,0,0],[0,0,0,0,0]]
+        # Test adding two 5x2 matrices
+        matrix_D1 = [[1,2],[3,4],[5,6],[7,8],[9,10]]
+        matrix_D2 = [[1,2],[3,4],[5,6],[7,8],[9,10]]
+
+        resultant_matrix_D = [[0,0],[0,0],[0,0],[0,0],[0,0]]
 
         self.assertEqual(matrix_toolkit.subtract_matrices(matrix_D1, matrix_D2), resultant_matrix_D)
+
+
+        # Test adding two 2x5 matrices
+        matrix_E1 = [[1,2,3,4,5],[6,7,8,9,10]]
+        matrix_E2 = [[1,2,3,4,5],[6,7,8,9,10]]
+
+        resultant_matrix_E = [[0,0,0,0,0],[0,0,0,0,0]]
+
+        self.assertEqual(matrix_toolkit.subtract_matrices(matrix_E1, matrix_E2), resultant_matrix_E)
     
+    # Test matrix_toolkit's scale_matrix method
     def test_scale_matrices(self): 
 
         # Test scaling a 2x2 matrix by 4
@@ -96,6 +116,7 @@ class TestMatrixToolkit(unittest.TestCase):
 
         self.assertEqual(matrix_toolkit.scale_matrix(matrix_B, scalar_B), resultant_matrix_B)
 
+
         # Test scaling a 5x2 matrix by -5
         scalar_C = -5
         matrix_C = [[1,2],[3,4],[5,6],[7,8],[9,10]]
@@ -103,6 +124,7 @@ class TestMatrixToolkit(unittest.TestCase):
         resultant_matrix_C = [[-5,-10],[-15,-20],[-25,-30],[-35,-40],[-45,-50]]
 
         self.assertEqual(matrix_toolkit.scale_matrix(matrix_C, scalar_C), resultant_matrix_C)
+
 
         # Test scaling a 2x5 matrix by 0
         scalar_D = 0
